c4a8cfce9167ea2d813b396692ef2abf655548be
Manish Patil <manish.patil2024@outlook.com>
2024-07-08 14:33:17 +0530

    adding conflit
    adding conflit

---
+++ src/main/java/com/subscription/controller/SubscriptionController.java
@@ -0,0 +1,101 @@
+package com.subscription.controller;
+
+import java.text.ParseException;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.subscription.model.Plan;
+import com.subscription.request.PaymentRequest;
+import com.subscription.request.UserRequest;
+import com.subscription.service.SubscriptionInterface;
+
+@RestController
+@RequestMapping("/razorpay")
+public class SubscriptionController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SubscriptionController.class);
+
+	private final SubscriptionInterface subscriptionInterface;
+	private final UserRequest userRequest;
+
+	public SubscriptionController(UserRequest userRequest, SubscriptionInterface subscriptionInterface) {
+		this.subscriptionInterface = subscriptionInterface;
+		this.userRequest = userRequest;
+	}
+
+	@PostMapping("/create/subscription/")
+	public String createSubscription(@RequestBody UserRequest request) {
+		try {
+			logger.info("Received request: {}", request.toString());
+			userRequest.setUser_id(request.getUser_id());
+			userRequest.setOrganization_id(request.getOrganization_id());
+			userRequest.setPlan_id(request.getPlan_id());
+
+			// Call get plan and return
+			Plan plan = subscriptionInterface.getPlan(request.getPlan_id());
+
+			// call createSubscription and return in JSON
+			JSONObject jsonResponse = subscriptionInterface.createSubscription(plan.getPlanIdRazorPayPlanId(),
+					plan.getMonthlyCycle(), request);
+
+			if (jsonResponse == null) {
+				jsonResponse.put("payment_error", "Payment server down. Please try again sometime :)");
+				return jsonResponse.toString();
+			}
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+
+			return jsonResponse.toString();
+		} catch (Exception e) {
+			logger.error("Error creating subscription");
+			JSONObject error = new JSONObject();
+			error.put("payment_error", "Payment server down. please try again sometime :)");
+			return error.toString(); 
+		}
+	}
+
+	@PostMapping("/verify/payment/")
+	public ResponseEntity<String> verifyPayment(@RequestBody PaymentRequest paymentRequest) throws ParseException {
+		System.out.println("get data " + paymentRequest);
+		JSONObject jsonResponse = subscriptionInterface.paymentVerifiction(paymentRequest.getSubscription_id(),
+				paymentRequest.getPayment_id(), paymentRequest.getUser_id(), paymentRequest.getOrganization_id());
+
+		if (jsonResponse == null) {
+			System.out.println("json is null...");
+			return ResponseEntity.noContent().build();
+		}
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		return ResponseEntity.ok().headers(headers).body(jsonResponse.toString());
+	}
+
+	// @PostMapping("/payment/receipt/")
+	@RequestMapping(value = "/payment/receipt/{subscriptionId}", method = RequestMethod.POST)
+	public ResponseEntity<?> PaymentHistory(@PathVariable("subscriptionId") String subscriptionId) {
+
+		JSONObject json = subscriptionInterface.getPymentHistory(subscriptionId);
+
+		if (json != null) {
+			return ResponseEntity.ok(json.toString());
+		}
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body("No content found");
+	}
+}
---
+++ src/main/java/com/subscription/service/SubscriptionService.java
@@ -0,0 +1,150 @@
+package com.subscription.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.razorpay.Payment;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import com.razorpay.Subscription;
+import com.subscription.model.InsertStatusOfPayment;
+import com.subscription.request.UserRequest;
+import com.subscription.response.PaymentResponseEntity;
+import com.subscription.response.SubscriptionResponseDTO;
+
+@Service
+public class SubscriptionService {
+
+	// private BitlyService bitlyService;
+
+	private final RazorpayClient razorpayClient;
+	private final UserRequest userRequest;
+	private final PaymentResponseEntity paymentResponse;
+	private int paymentStatusId;
+	// private Boolean captured;
+	private String status;
+	@Autowired
+	InsertStatusOfPayment insertStatusOfPayment;
+	@Autowired
+	PaymentStatusOnOrganizationLevel paymentStatusOnOrganizationLevel;
+
+	// @Autowired
+	public SubscriptionService(RazorpayClient razorpayClient, UserRequest userRequest,
+			PaymentResponseEntity paymentResponse) {
+		this.razorpayClient = razorpayClient;
+		this.userRequest = userRequest;
+		this.paymentResponse = paymentResponse;
+
+	}
+
+	public SubscriptionResponseDTO createSubscriptionAndInsertDB(String PlanId, int MonthlyCycle, UserRequest user)
+			throws Exception, RazorpayException {
+
+		JSONObject subscriptionRequest = new JSONObject();
+		subscriptionRequest.put("plan_id", PlanId);
+		subscriptionRequest.put("total_count", MonthlyCycle);
+		subscriptionRequest.put("quantity", 1);
+		subscriptionRequest.put("customer_notify", 0);
+//        
+//        //Create Subscription
+		Subscription subscription = razorpayClient.subscriptions.create(subscriptionRequest);
+//       
+//        // Set Data in model class  SubscriptionResponseDTO fetching from  RazorPay Subscription Class
+		SubscriptionResponseDTO responseDTO = new SubscriptionResponseDTO();
+		responseDTO.setRazorPaySubscriptionId(subscription.get("id"));
+		status = subscription.get("status");
+		responseDTO.setSubscriptionStatusId(EnumMappingService.getSubscriptionStatusByString(status));
+		responseDTO.setSubscriptionLink(subscription.get("short_url"));
+
+//        // Get Data In UserRequest and set in SubscriptionResponseDTO
+//      
+		System.out.println(userRequest);
+		responseDTO.setUserId(userRequest.getUser_id());
+		responseDTO.setPlanId(userRequest.getPlan_id());
+		responseDTO.setOrganizationId(userRequest.getOrganization_id());
+
+		return responseDTO;
+	}
+
+	public SubscriptionResponseDTO FecthSubcriptionDetails(String subscriptionId)
+			throws RazorpayException, ParseException {
+		SubscriptionResponseDTO responseDTO = new SubscriptionResponseDTO();
+		Subscription subscription = razorpayClient.subscriptions.fetch(subscriptionId);
+		responseDTO.setRazorPaySubscriptionId(subscriptionId);
+		status = subscription.get("status");
+		responseDTO.setSubscriptionStatusId(EnumMappingService.getSubscriptionStatusByString(status));
+		System.out.println("current_start");
+		responseDTO.setStartDate(convertUnixTimeToDate(subscription.get("current_start")));
+		System.out.println("charge_at");
+		responseDTO.setNextDueDate(convertUnixTimeToDate(subscription.get("charge_at")));
+		System.out.println("end_at");
+		responseDTO.setEndDate(convertUnixTimeToDate(subscription.get("end_at")));
+		responseDTO.setSubscriptionLink(subscription.get("short_url"));
+
+		UserRequest user = new UserRequest();
+		System.out.println(user);
+		return responseDTO;
+
+	}
+
+	public JSONObject VerificationService(String razorpaySuscriptionId, String paymentId, Long userId,
+			Long organizationId) throws RazorpayException, ParseException {
+//    	feching payment details
+		Payment razorPayPayment = razorpayClient.payments.fetch(paymentId);
+
+		// Set Data in PaymentEntity
+		paymentResponse.setRazorpayPaymentId(paymentId);
+		paymentResponse.setRazorpayOrderId(razorPayPayment.get("order_id"));
+		paymentResponse.setRazorPaySubscriptionId(razorpaySuscriptionId);
+		paymentResponse.setUserId(userId);
+		paymentResponse.setOrganizationId(organizationId);
+		// captured and Status fetch
+		Boolean captured = razorPayPayment.get("captured");
+		String status = razorPayPayment.get("status");
+		paymentStatusId = EnumMappingService.getPaymentStatus(captured, status);
+		JSONObject jsonResponseObject = EnumMappingService.paymentResponse(paymentStatusId, razorPayPayment);
+		paymentResponse.setPaymentStatusId(paymentStatusId);
+		paymentResponse.setAmount(razorPayPayment.get("amount"));
+		paymentResponse.setPaymentMode(razorPayPayment.get("method"));
+		paymentResponse.setUserEmail(razorPayPayment.get("email"));
+		paymentResponse.setContact(razorPayPayment.get("contact"));
+		paymentResponse.setCreatedOn(razorPayPayment.get("created_at"));
+		if (status.equals("captured")) {
+			insertStatusOfPayment.setIsPaid((byte) 1);
+			insertStatusOfPayment.setOrganization_id(organizationId);
+		//	System.out.println("isPaid Update date");
+			paymentStatusOnOrganizationLevel.setPaymentStatusOnOrgainaztion();
+			
+			
+		}
+
+		JSONObject transecationId = razorPayPayment.get("acquirer_data");
+		Object rrn = transecationId.get("rrn");
+		System.out.println("rrn = " + rrn);
+		if (!rrn.equals(null)) {
+			paymentResponse.setTransactionId(transecationId.getString("upi_transaction_id"));
+		}
+		System.out.println(paymentResponse);
+		return jsonResponseObject;
+	}
+
+	private Date convertUnixTimeToDate(Object unixTime) throws ParseException {
+		System.out.println(unixTime);
+		long timestamp = Long.parseLong(unixTime.toString()) * 1000L;
+		Date date = new Date(timestamp);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		sdf.setTimeZone(TimeZone.getDefault());
+		String localDateString = sdf.format(date);
+		return sdf.parse(localDateString);
+
+	}
+
+    
+    
+}
